9dc3ccc878000b41ca3b72c5989eea57a3bd3482
Adetayo <twinebravo@gmail.com>
2024-07-09 05:50:11 +0100

    Added java file

---
+++ Calculator.java
@@ -0,0 +1,46 @@
+public class Calculator{
+
+
+public int add(int firstNumber, int secondNumber){
+		
+		int sum = firstNumber + secondNumber;
+
+		return sum;
+}
+
+
+public int subtract(int firstNumber, int secondNumber){
+
+		int difference = firstNumber - secondNumber;
+
+		return difference;
+}
+
+
+public int divide(int firstNumber, int secondNumber){
+		
+		int quotient = firstNumber / secondNumber;
+
+		return quotient;
+}
+
+
+public int multiply(int firstNumber, int secondNumber){
+		
+		int factor = firstNumber * secondNumber;
+
+		return factor;
+}
+
+/*
+public int divisible(int firstNumber, int secondNumber){
+		
+		int result = firstNumber / secondNumber;
+
+		return result;
+}
+*/
+
+
+
+}
